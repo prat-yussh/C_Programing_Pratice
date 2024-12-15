655171e824986ee93022c1d3d45edf7227d513d5
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-15 15:43:27 +0530

    Ongoing

--- Chapter_5/Chapter5_Q3.c
+++ Chapter_5/Chapter5_Q3.c
@@ -3,9 +3,20 @@ Question:-3
 Write a function to calculate force of attraction on a body of mass ‘m’ exerted by 
 earth. Consider g = 9.8m/s2.
 */
+
 #include <stdio.h>
+int ForceOfAttraction(float);
+
+int ForceOfAttraction(float m)
+{
+    const float g=9.8;
+    float ForceOfAttraction=m*g;
+    printf("ForceOfAttraction:%f", ForceOfAttraction);
+    return ForceOfAttraction;
+}
+
 int main()
 {
-    
+    ForceOfAttraction(45.0);
     return 0;
 }
