59e248261f0533e076bc7db213d348a66425456c
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-17 15:50:34 +0530

    Completed

--- Chapter_5/Chapter5_Q6.c
+++ Chapter_5/Chapter5_Q6.c
@@ -4,30 +4,25 @@ Write a recursive function to calculate the sum of first ‘n’ natural numbers
 */
 #include <stdio.h>
 
-// int sum(int);
+int sum(int);
 
-// int sum(int n)
-// {
-//     if (n == 1 || n == 2)
-//     {
-//         return n;
-//     }
-//     // int add=sum(n)+sum(n-1);
-//     return sum(n) + sum(n - 1);
-// }
+int sum(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    int AddOfNum=n+sum(n-1);
+    return AddOfNum;
+}
 
 int main()
 {
+    int a;
+    printf("ENter a number to calculate the sum of first 'n' natural numbers");
+    scanf("%d", &a);
 
-    int n = 3,sum;
-    // printf("ENter a number to calculate the sum of first 'n' natural numbers");
-    // scanf("%d", &a);
-    for (int i = 0; i < n; i++)
-    {
-        sum = n + n - 1;
-    }
-
-    printf("The sum of first 'n' natural numbers is:%d", sum);
+    printf("The sum of first '%d' natural numbers is:%d", a, sum(a));
 
     return 0;
 }
