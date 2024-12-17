8570415b55acaa508c386feffe5bbac45dc11383
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-17 16:11:37 +0530

    Completed

--- Chapter_5/Chapter5_Q7.c
+++ Chapter_5/Chapter5_Q7.c
@@ -1,9 +1,31 @@
 /*
-
+Write a program using function to print the following pattern (first n lines)
+*
+* * *
+* * * * *
 */
 #include <stdio.h>
-int main()
+
+int display(int);
+
+int display(int n)
 {
     
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n = 3;
+
+    display(n);
+
     return 0;
 }
