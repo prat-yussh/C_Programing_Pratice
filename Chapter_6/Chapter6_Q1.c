c1f4be74f546b8314599e8a56a56c7dea07fb795
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-18 14:29:46 +0530

    Completed

--- Chapter_6/Chapter6_Q1.c
+++ Chapter_6/Chapter6_Q1.c
@@ -6,5 +6,8 @@ Write a program to print the address of a variable. Use this address to get the 
 int main()
 {
     int a=7;
+    int* b=&a;
+    printf("The addres of a is %p\n", b);
+    printf("The value of a using adrees of a is %d", *(&a));
     return 0;
 }
