902a105e4a02331db83c16ccc3b66d5be9d7104b
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-18 20:51:43 +0530

    Completed

--- Chapter_6/Chapter6_Q7.c
+++ Chapter_6/Chapter6_Q7.c
@@ -7,8 +7,18 @@ Try problem 3 using call by value and verify that it does not change the value o
 the said variable.
 */
 #include <stdio.h>
+void ChangeTo10Times(int);
+
+void ChangeTo10Times(int a)
+{
+    a=a*10;
+}
+
 int main()
 {
-    
+    int a=10;
+    printf("The value of a %d\n", a);
+    ChangeTo10Times(a);
+    printf("The value of a %d", a);
     return 0;
 }
