4c1ef9294ea9f0666b8f761e68c3f5b0879f6f1d
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-18 14:23:16 +0530

    Completed

---
+++ Chapter_6/Chapter6_Q1.c
@@ -0,0 +1,10 @@
+/*
+Question:-1
+Write a program to print the address of a variable. Use this address to get the value of the variable.
+*/
+#include <stdio.h>
+int main()
+{
+    int a=7;
+    return 0;
+}
