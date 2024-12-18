c63a862743387b8a844f350cd6240c10c4f9e9e5
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-18 20:21:26 +0530

    Completed

---
+++ Chapter_6/Chapter6_Q7.c
@@ -0,0 +1,14 @@
+/*
+Question:-7
+3. Write a program to change the value of a variable to ten times of its current 
+value.
+
+Try problem 3 using call by value and verify that it does not change the value of 
+the said variable.
+*/
+#include <stdio.h>
+int main()
+{
+    
+    return 0;
+}
