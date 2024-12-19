40d8d5126ec016cb4510882f9eaee762349f1d19
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-19 20:37:24 +0530

    Completed

---
+++ Chapter_7/Chapter7_Q1.c
@@ -0,0 +1,20 @@
+/*
+
+*/
+#include <stdio.h>
+int main()
+{
+    int arr[5];
+    printf("Enter value");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    
+    return 0;
+}
