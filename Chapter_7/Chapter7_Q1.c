febe37f36c68df82226f243bf0735320c7e08209
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-20 13:58:22 +0530

    Completed

--- Chapter_7/Chapter7_Q1.c
+++ Chapter_7/Chapter7_Q1.c
@@ -1,28 +1,16 @@
 /*
- Create a 2-d array by taking input from the user. Write a display function to 
-print the content of this 2-d array on the screen.
+Question:-1
+Create an array of 10 numbers. Verify using pointer arithmetic that (ptr+2) points 
+to the third element where ptr is a pointer pointing to the first element of the 
+array.
 */
 #include <stdio.h>
 int main()
 {
-    int arr[3][2];
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            printf("Enter The value[%d][%d]", i, j);
-            scanf("%d", &arr[i][j]);
-        }
-        
-    }
-    
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            printf("the value is [%d][%d]=%d\n", i, j, arr[i][j]);
-        }
-        
-    }
+    int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int* ptr=arr;
+    printf("Here the ptr points to the first element ptr=%d\n", *ptr);
+    ptr=ptr+2;
+    printf("Here the ptr points to the 3rd element after addition '(ptr+2)' ptr=%d", *ptr);
     return 0;
 }
