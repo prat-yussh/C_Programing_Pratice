993a37c3db9cc22028005a9f462c24b59c49fd91
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-20 20:45:01 +0530

    Completed

--- Chapter_7/Chapter7_Q3.c
+++ Chapter_7/Chapter7_Q3.c
@@ -1,11 +1,18 @@
 /*
 Question:-3
-Write a program to create an array of 10 integers and store multiplication table of 
+Write a program to create an array of 10 integers and store multiplication table of
 5 in it.
 */
 #include <stdio.h>
 int main()
 {
-    
+    int arr[10];
+    for (int i = 1; i < 11; i++)
+    {
+        int a = 8;
+        int mul = a * i;
+        printf("VAlue at index [%d] is =%d\n", i, mul);
+    }
+
     return 0;
 }
