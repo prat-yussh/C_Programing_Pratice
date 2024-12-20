3e7783042d302acc00b8f0e341464d1ac51210d5
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-20 20:22:45 +0530

    Completed

--- Chapter_7/Chapter7_Q2.c
+++ Chapter_7/Chapter7_Q2.c
@@ -1,4 +1,5 @@
 /*
+Question:-2
 If S[3] is a 1-D array of integers then *(S+3) refers to the third element: 
 (i) True.  
 (ii) False.  
@@ -9,7 +10,7 @@ int main()
 {
     int S[3]={1,2,3};
     int* ptr=S;
-    ptr=S+2;
+    ptr=S+3;  //The answer is false beacuse the index start from zero and we defined a 1d array with 3 contiguos memory the 'S' is by default defined to the 1st element and after '(S+3)' s is defined to the fourth elment but defined our array as 3 memory
     printf("The value is %d", *ptr);
     return 0;
 }
---
+++ Chapter_7/Chapter7_Q3.c
@@ -0,0 +1,11 @@
+/*
+Question:-3
+Write a program to create an array of 10 integers and store multiplication table of 
+5 in it.
+*/
+#include <stdio.h>
+int main()
+{
+    
+    return 0;
+}
