a90641513ed484069e2fbaf5d754219f92543d01
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-21 22:12:49 +0530

    Completed

--- Chapter_7/Chapter7_Q2.c
+++ Chapter_7/Chapter7_Q2.c
@@ -11,6 +11,6 @@ int main()
     int S[3]={1,2,3};
     int* ptr=S;
     ptr=S+3;  //The answer is false beacuse the index start from zero and we defined a 1d array with 3 contiguos memory the 'S' is by default defined to the 1st element and after '(S+3)' s is defined to the fourth elment but defined our array as 3 memory
-    printf("The value is %d", *ptr);
+    printf("The vaalue is %d", *ptr);
     return 0;
 }
