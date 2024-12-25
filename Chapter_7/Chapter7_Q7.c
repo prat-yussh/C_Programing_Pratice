10ff4827df732a4b2df91b8da31ac2d06e59d5e2
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-25 20:44:04 +0530

    Completed

--- Chapter_7/Chapter7_Q7.c
+++ Chapter_7/Chapter7_Q7.c
@@ -8,39 +8,22 @@ and 9 respectively.
 int main()
 {
     int arr[3][10];
-    int mul[] = {2, 7, 9};
+    int mul[3]={2,7,9};
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 1; j < 11; j++)
         {
-            arr[i][j] = mul[i] * (j + 1);
-        }
+            arr[i][j]=mul[i] * j;
+        }   
     }
 
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 1; j < 11; j++)
         {
-            printf("The value of arr[%d][%d] is %d\n", i, j, arr[i][j]);
-        }
-        printf("\n");
+            printf("Multiplication Table = [%d][%d]=%d\n", i, j, arr[i][j]);
+        }  
+        printf("\n"); 
     }
-
     return 0;
 }
-// #include <stdio.h>
-
-// int main()
-// {
-//     int arr[3][10];
-//     int mul[3]={2,7,9};
-//     for (int i = 0; i < 3; i++)
-//     {
-//         for (int j = 0; j < 10; j++)
-//         {
-//             arr[i][j]=mul   
-//         }
-        
-//     }
-//     return 0;
-// }
