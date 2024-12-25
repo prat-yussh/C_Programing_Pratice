53aef9984f3a07058dc95df77bdda0515686611d
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-25 13:34:24 +0530

    Completed

--- Chapter_7/Chapter7_Q7.c
+++ Chapter_7/Chapter7_Q7.c
@@ -1,19 +1,44 @@
 /*
 Question:-7
-Create an array of size 3 x 10 containing multiplication tables of the numbers 2,7 
+Create an array of size 3 x 10 containing multiplication tables of the numbers 2,7
 and 9 respectively.
 */
 #include <stdio.h>
 
+int mul(int);
+
+int mul(int x)
+{
+    for (int i = 2; i < 10; i++)
+    {
+        if (i == 2)
+        {
+            for (int j = 1; j < 11; j++)
+            {
+                printf("%d * %d = %d\n", i, j, i * j);
+            }
+        }
+        else if (i == 7)
+        {
+            for (int j = 1; j < 11; j++)
+            {
+                printf("%d * %d = %d\n", i, j, i * j);
+            }
+        }
+        else if (i == 9)
+        {
+            for (int j = 1; j < 11; j++)
+            {
+                printf("%d * %d = %d\n", i, j, i * j);
+            }
+        }
+    }
+}
+
 int main()
 {
     int arr[3][10];
-    printf("Enter a number");
-    scanf("%d", &arr);
-    for (int i = 0; i < a7; i++)
-    {
-        printf("the table %d * %d = %d", num, i , num*i);
-    }
-    
+    mul(arr[3][10]);
+
     return 0;
 }
