0e08ebd03fb5632a0bfb362672d9b488d8aa145e
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-26 20:10:10 +0530

    Completd

---
+++ Chapter_7/Chapter7_Q9.c
@@ -0,0 +1,24 @@
+/*
+Question:-9
+Create a three–dimensional array and print the address of its elements in 
+increasing order.
+*/
+#include <stdio.h>
+int main()
+{
+    int arr[2][2][2]={{1,2},{3,4},{5,6}};
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < 2; k++)
+            {
+                printf("arr[%d][%d][%d]=%d\n", i, j, k, arr[i][j][k]);
+            }
+            
+        }
+        
+    }
+    
+    return 0;
+}
