222a97eba763498a8db52afde03c5a7b1b4b960e
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-26 22:22:46 +0530

    Completed

--- Chapter_7/Chapter7_Q9.c
+++ Chapter_7/Chapter7_Q9.c
@@ -1,12 +1,15 @@
 /*
 Question:-9
-Create a three–dimensional array and print the address of its elements in 
+Create a three–dimensional array and print the address of its elements in
 increasing order.
 */
 #include <stdio.h>
 int main()
 {
-    int arr[2][2][2];
+    int arr[2][2][2] = {{1, 2},
+                        {3, 4}},
+        {{5, 6},
+         {7, 8}};
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
@@ -15,10 +18,8 @@ int main()
             {
                 printf("arr[%d][%d][%d]=%u\n", i, j, k, &arr[i][j][k]);
             }
-            
         }
-        
     }
-    
+
     return 0;
 }
