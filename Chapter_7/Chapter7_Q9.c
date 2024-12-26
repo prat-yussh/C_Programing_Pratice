e2dae821a3fb374a77d7f63c1baaf3ad66875879
pratyush3112 <pratyushkumar3m@gmail.com>
2024-12-26 22:24:08 +0530

    Completed

--- Chapter_7/Chapter7_Q9.c
+++ Chapter_7/Chapter7_Q9.c
@@ -6,10 +6,10 @@ increasing order.
 #include <stdio.h>
 int main()
 {
-    int arr[2][2][2] = {{1, 2},
-                        {3, 4}},
-        {{5, 6},
-         {7, 8}};
+    int arr[2][2][2] = {{{1, 2},
+                        {3, 4}}},
+        {{{5, 6},
+         {7, 8}}};
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
