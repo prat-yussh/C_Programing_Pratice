e53624d4037884c9238779b151c6248397782a7b
pratyush3112 <pratyushkumar3m@gmail.com>
2025-01-02 20:09:00 +0530

    Completed

---
+++ Chapter_8/Chapter8_Q1.c
@@ -0,0 +1,16 @@
+/*
+
+*/
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    // char st[]={'a','b','c','\0'};
+    char st[]="harry";
+    for(int i=0;i<5;i++)
+    {
+    printf("%c", st[i]);
+
+    }
+    return 0;
+}
