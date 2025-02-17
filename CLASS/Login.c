4c0177e3be3bb37e80f2e91ba4fb739f7c760ec6
pratyush3112 <pratyushkumar3m@gmail.com>
2025-02-17 16:30:44 +0530

    1

--- CLASS/Login.c
+++ CLASS/Login.c
@@ -1,86 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_USERS 100
+#define USERNAME "omm"
+#define PASSWORD "230301121561"
 
-typedef struct {
-    char id[20];
-    char password[20];
-} User;
-
-User users[MAX_USERS];
-int userCount = 0;
-
-void registerUser() {
-    if (userCount >= MAX_USERS) {
-        printf("User limit reached. Cannot register more users.\n");
-        return;
-    }
-
-    char id[20], password[20];
-    printf("Enter a new ID: ");
-    scanf("%s", id);
-    printf("Enter a new Password: ");
-    scanf("%s", password);
-
-    for (int i = 0; i < userCount; i++) {
-        if (strcmp(users[i].id, id) == 0) {
-            printf("User ID already exists. Please try a different ID.\n");
-            return;
-        }
-    }
-
-    strcpy(users[userCount].id, id);
-    strcpy(users[userCount].password, password);
-    userCount++;
-    printf("Registered successfully with ID %s\n", id);
-}
-
-void login() {
-    char id[20], password[20];
-    printf("Enter your ID: ");
-    scanf("%s", id);
-    printf("Enter your Password: ");
-    scanf("%s", password);
-
-    for (int i = 0; i < userCount; i++) {
-        if (strcmp(users[i].id, id) == 0) {
-            if (strcmp(users[i].password, password) == 0) {
-                printf("Login successful as %s\n", id);
-                return;
-            } else {
-                printf("Sorry, password incorrect.\n");
-                return;
-            }
-        }
-    }
-    printf("Invaalid ID or Password. Please try again.\n");
-}
-
-int main() {
+void showMenu() {
     int choice;
-    do {
-        printf("\nMenu:\n");
-        printf("1. Login\n");
-        printf("2. Register\n");
-        printf("3. Exit\n");
+
+    while (1) {
+        printf("\n===== MENU =====\n");
+        printf("1. Tea\n");
+        printf("2. Biryani\n");
+        printf("3. Veg Meals\n");
+        printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                login();
+                printf("\nYou selected Tea. Enjoy your drink!\n");
                 break;
             case 2:
-                registerUser();
+                printf("\nYou selected Biryani. Spicy choice!\n");
                 break;
             case 3:
-                printf("Exiting...\n");
+                printf("\nYou selected Veg Meals. Healthy and tasty!\n");
                 break;
+            case 4:
+                printf("\nExiting... Have a great day!\n");
+                return;
             default:
-                printf("Invalid choice. Please try again.\n");
+                printf("\nInvalid choice! Please enter a valid option.\n");
         }
-    } while (choice != 3);
+    }
+}
+
+int main() {
+    char inputUsername[20], inputPassword[20];
+
+    printf("Enter Username: ");
+    scanf("%s", inputUsername);
+
+    getchar(); 
+    printf("Enter Password: ");
+    fgets(inputPassword, sizeof(inputPassword), stdin);
+    inputPassword[strcspn(inputPassword, "\n")] = 0; 
+
+    if (strcmp(inputUsername, USERNAME) == 0 && strcmp(inputPassword, PASSWORD) == 0) {
+        printf("\nLogin Successful!\n");
+        showMenu();
+    } else {
+        printf("\nAccess Denied: Invalid Username or Password.\n");
+    }
 
     return 0;
 }
