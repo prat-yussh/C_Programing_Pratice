ed1a842793df33c11ba2ae4769cebd0a51cf72c8
pratyush3112 <pratyushkumar3m@gmail.com>
2025-03-10 16:50:04 +0530

    lcs

--- CLASS/Login.c
+++ CLASS/Login.c
@@ -1,58 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-void LCS(char *X, char *Y, int m, int n)
-{
+void LCS(char *X, char *Y, int m, int n) {
     int c[m + 1][n + 1];
     char b[m + 1][n + 1];
 
-    for (int i = 0; i <= m; i++)
-        c[i][0] = 0;
-    for (int j = 0; j <= n; j++)
-        c[0][j] = 0;
-
-    for (int i = 1; i <= m; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            if (X[i - 1] == Y[j - 1])
-            {
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i == 0 || j == 0) {
+                c[i][j] = 0;
+                b[i][j] = '0';
+            } else if (X[i - 1] == Y[j - 1]) {
                 c[i][j] = c[i - 1][j - 1] + 1;
                 b[i][j] = '\\';
-            }
-            else if (c[i - 1][j] >= c[i][j - 1])
-            {
+            } else if (c[i - 1][j] >= c[i][j - 1]) {
                 c[i][j] = c[i - 1][j];
                 b[i][j] = '|';
-            }
-            else
-            {
+            } else {
                 c[i][j] = c[i][j - 1];
                 b[i][j] = '-';
             }
         }
     }
 
-    printf("Length of LCS: %d\n", c[m][n]);
-
     int i = m, j = n;
     int index = c[m][n];
     char lcs[index + 1];
     lcs[index] = '\0';
-    while (i > 0 && j > 0)
-    {
-        if (b[i][j] == '\\')
-        {
+    while (i > 0 && j > 0) {
+        if (b[i][j] == '\\') {
             lcs[--index] = X[i - 1];
             i--;
             j--;
-        }
-        else if (b[i][j] == '|')
-        {
+        } else if (b[i][j] == '|') {
             i--;
-        }
-        else
-        {
+        } else {
             j--;
         }
     }
@@ -60,8 +42,7 @@ void LCS(char *X, char *Y, int m, int n)
     printf("LCS: %s\n", lcs);
 }
 
-int main()
-{
+int main() {
     char X[] = "ABCBDAB";
     char Y[] = "BDCABA";
     int m = strlen(X);
